7a4e4af1e5afdd21155fd7e223d6ca78f4e8710d
Brian <brian@brianaderer.com>
2024-04-21 12:39:54 -0400

    this is gonna fail

--- singleton.py
+++ singleton.py
@@ -1,5 +1,4 @@
 from utilities.parser import Parser
-from utilities.parser2 import Parser2
 from utilities.firebase import Firebase
 from utilities.save import Save
 
@@ -13,7 +12,7 @@ class Singleton:
             cls._instance = super(Singleton, cls).__new__(cls)
             # Instantiate and attach child classes directly
             cls._instance.Parser = Parser()
-            cls._instance.Parser2 = Parser2()
+            # cls._instance.Parser2 = Parser2()
             cls._instance.Firebase = Firebase()
             cls._instance.Save = Save()
         return cls._instance
